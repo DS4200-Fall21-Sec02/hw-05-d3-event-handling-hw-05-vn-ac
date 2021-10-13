c45a4b0f033a946d8c7d273ae73f83c118890b9a
allychao <73146562+allychao@users.noreply.github.com>
2021-10-13 14:10:40 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,34 +5,53 @@ let margin = {
     top: 60,
     left: 50,
     right: 30,
-    bottom: 35
+    bottom: 35,
   },
   width = 500 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom;
 
-//SVG that will hold the visualization 
-let svg = d3.select('#vis')
-  .append('svg')
-  .attr('preserveAspectRatio', 'xMidYMid meet') // this will scale your visualization according to the size of its parent element and the page.
-  .attr('width', '100%') // this is now required by Chrome to ensure the SVG shows up at all
-  .style('background-color', 'white') 
-  .style('border', 'solid')
-  .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
+//SVG that will hold the visualization
+let svg = d3
+  .select("#vis")
+  .append("svg")
+  .attr("preserveAspectRatio", "xMidYMid meet") // this will scale your visualization according to the size of its parent element and the page.
+  .attr("width", "100%") // this is now required by Chrome to ensure the SVG shows up at all
+  .style("background-color", "white")
+  .style("border", "solid")
+  .attr(
+    "viewBox",
+    [
+      0,
+      0,
+      width + margin.left + margin.right,
+      height + margin.top + margin.bottom,
+    ].join(" ")
+  );
 
-// Add a square 
-let rect = svg.append('rect')
-  .attr('x', '100')
-  .attr('y', '200')
-  .attr('width', '20%')
-  .attr('height', '20%')
-  .attr('fill', '#a6cee3'); 
+// function for changing color
+var changeColor = (function () {
+  var currentColor = "white";
 
-// Add a circle 
-let circle = svg.append('circle') 
-  .attr('cx', '350')
-  .attr('cy', '250')
-  .attr('r', '60')
-  .attr('fill', '#b2df8a')
+  return function () {
+    currentColor = currentColor == "white" ? "magenta" : "white";
+    d3.select(this).style("fill", currentColor);
+  };
+})();
 
+// Add a square
+let rect = svg
+  .append("rect")
+  .attr("x", "100")
+  .attr("y", "200")
+  .attr("width", "20%")
+  .attr("height", "20%")
+  .attr("fill", "#a6cee3")
+  .on("click", changeColor);
 
-
+// Add a circle
+let circle = svg
+  .append("circle")
+  .attr("cx", "350")
+  .attr("cy", "250")
+  .attr("r", "60")
+  .attr("fill", "#b2df8a");
